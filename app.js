e58de0d293dcb069595adaee93ba13406c0f7fbd
Chris Welch <deadclap893@gmail.com>
2017-07-27 18:42:36 -0400

    [app.js] testing

--- app.js
+++ app.js
@@ -3,19 +3,19 @@ const komada = require('komada');
  var oID = "195223544186142727"
 var perms = new komada.PermLevels()
 .addLevel(0, false, () => true)
-.addLevel(2, false, (client,msg) => {
+.addLevel(2, false, (client, msg) => {
   var modRole = msg.guild.roles.get(client.settingGateway.get(msg.guild.id).modRole)
   return modRole && msg.member.roles.has(modRole)
 })
-.addLevel(3, false, (client,msg) => {
+.addLevel(3, false, (client, msg) => {
   var adminRole = msg.guild.roles.get(client.settingGateway.get(msg.guild.id).adminRole)
   return adminRole && msg.member.roles.has(adminRole)
 })
-.addLevel(4, false, (client,msg) => {
+.addLevel(4, false, (client, msg) => {
   return msg.author.id === msg.guild.ownerID
 })
-.addLevel(10, false, (client,msg) => {
-  return msg.author.id === oID || eCFG.aditionalOwners.includes(msg.author.id)
+.addLevel(10, false, (client, msg) => {
+  return msg.author.id === oID //|| eCFG.aditionalOwners.includes(msg.author.id)
 })
 .structure
 
