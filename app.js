dc034e2dc3088c2ffd5dfde0a12f0f28f7b780a6
Chris Welch <deadclap893@gmail.com>
2017-07-27 06:24:39 -0400

    [app.js] Def PS for now!

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
 const komada = require('komada');
  var eCFG = require('./extraCFG.json')
  var oID = "195223544186142727"
-var perms = new komada.PermLevels()
-.addLevel(0, false, () => true)
-.addLevel(2, false, (client,msg) => {
-  var modRole = msg.guild.roles.get(client.settingGateway.get(msg.guild.id).modRole)
-  if (modRole && msg.member.roles.has(modRole)) return true
-})
-.addLevel(3, false, (client,msg) => {
-  var adminRole = msg.guild.roles.get(client.settingGateway.get(msg.guild.id).adminRole)
-  if (adminRole && msg.member.roles.has(adminRole)) return true
-})
-.addLevel(4, false, (client,msg) => {
-  if (msg.author.id === msg.guild.ownerID) return true
-})
-.addLevel(10, false, (client,msg) => {
-  if (msg.author.id === oID || eCFG.aditionalOwners.includes(msg.author.id)) return true
-})
-.structure
+// var perms = new komada.PermLevels()
+// .addLevel(0, false, () => true)
+// .addLevel(2, false, (client,msg) => {
+//   var modRole = msg.guild.roles.get(client.settingGateway.get(msg.guild.id).modRole)
+//   if (modRole && msg.member.roles.has(modRole)) return true
+// })
+// .addLevel(3, false, (client,msg) => {
+//   var adminRole = msg.guild.roles.get(client.settingGateway.get(msg.guild.id).adminRole)
+//   if (adminRole && msg.member.roles.has(adminRole)) return true
+// })
+// .addLevel(4, false, (client,msg) => {
+//   if (msg.author.id === msg.guild.ownerID) return true
+// })
+// .addLevel(10, false, (client,msg) => {
+//   if (msg.author.id === oID || eCFG.aditionalOwners.includes(msg.author.id)) return true
+// })
+// .structure
 
 var test = new komada.Client({
   ownerID: oID,
@@ -25,8 +25,8 @@ var test = new komada.Client({
   clientOptions: {
     fetchAllMembers: true
   },
-  permStructure: perms,
-  cmdEditing: true,
+  // permStructure: perms,
+  commandEditing: true,
   cmdLogging: true,
   extraCFG: eCFG
 })
