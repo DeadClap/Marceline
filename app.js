33ebc3d173f186b8d6c87b19280ba6b4b5c1d4b8
Chris Welch <deadclap893@gmail.com>
2017-07-27 03:23:02 -0400

    [app.js] Perm Structure

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
 const komada = require('komada');
+ var eCFG = require('./extraCFG.json')
+ var oID = "195223544186142727"
+var perms = new Komada.PermLevels()
+.addLevel(0, false, () => true)
+.addLevel(2, false, (client,msg) => {
+  var modRole = msg.guild.roles.get(client.settingGateway.get(msg.guild.id).modRole)
+  if (modRole && msg.member.roles.has(modRole)) return true
+})
+.addLevel(3, false, (client,msg) => {
+  var adminRole = msg.guild.roles.get(client.settingGateway.get(msg.guild.id).adminRole)
+  if (adminRole && msg.member.roles.has(adminRole)) return true
+})
+.addLevel(4, false, (client,msg) => {
+  if (msg.author.id === msg.guild.ownerID)
+})
+.addLevel(10, false, (client,msg) => {
+  if (msg.author.id === oID || eCFG.aditionalOwners.includes(msg.author.id)) return true
+})
+.structure
 
 var test = new komada.Client({
-  ownerID: "195223544186142727",
+  ownerID: oID,
   prefix: "y.",
   clientOptions: {
     fetchAllMembers: true
   },
   cmdEditing: true,
   cmdLogging: true,
-  extraCFG: require('./extraCFG.json')
+  extraCFG: eCFG
 })
 test.login(require('../bot_logins.json').MBeta)
