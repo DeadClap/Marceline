b1b4aff082d95a5f920654fe8bbb4e84c099d535
Chris Welch <deadclap893@gmail.com>
2017-07-27 18:49:07 -0400

    [app.js] Testing x2

--- app.js
+++ app.js
@@ -5,11 +5,11 @@ var perms = new komada.PermLevels()
 .addLevel(0, false, () => true)
 .addLevel(2, false, (client, msg) => {
   var modRole = msg.guild.roles.get(client.settingGateway.get(msg.guild.id).modRole)
-  return modRole && msg.member.roles.has(modRole)
+  return modRole && msg.member.roles.has(modRole.id)
 })
 .addLevel(3, false, (client, msg) => {
   var adminRole = msg.guild.roles.get(client.settingGateway.get(msg.guild.id).adminRole)
-  return adminRole && msg.member.roles.has(adminRole)
+  return adminRole && msg.member.roles.has(adminRole.id)
 })
 .addLevel(4, false, (client, msg) => {
   return msg.author.id === msg.guild.ownerID
